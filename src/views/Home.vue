c2bda23318c6ca8418b3f3a3f27d845aef701b6d
dannyrb <danny.ri.brown@gmail.com>
2018-05-10 13:19:07 -0400

    Start playing with Card decks and card deck row containers

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,13 +2,35 @@
   <div class="home">
 	  <cdd-row>
 		<cdd-card style="display: flex; flex-wrap: wrap;">
-			<section style="padding: 0 30px;">
-				<h2>Video Series</h2>
-				<cdd-card-deck />
-				<h2>Quick Clips</h2>
-				<cdd-card-deck />
-				<h2>Medium Posts</h2>
+
+			<h2 style="margin-top: 55px; margin-bottom: 15px;">Video Series</h2>
+			<section style="display: flex; flex-direction: row;">			
+				<!-- Navigate Left -->
+				<!-- TODO -->
+
+				
+				
+				<div class="card-deck-container">
+					<template
+						v-for="(x, i) in [1, 2, 3, 4, 5, 6, 7, 8, 9]"
+					>
+						<cdd-card-deck class="card-deck-item" :key="i" />
+					</template>
+				</div>
+
+				<!-- Navigate Right -->
+				<!-- TODO: -->
 			</section>
+
+				<h2 style="margin-top: 55px; margin-bottom: 15px;">Quick Clips</h2>
+				<div class="card-deck-container">
+					<template
+						v-for="(x, i) in [1, 2, 3, 4]"
+					>
+						<cdd-card-deck class="card-deck-item" :key="i" />
+					</template>
+				</div>
+				<h2>Medium Posts</h2>
 		</cdd-card>
 	  </cdd-row>
   </div>
@@ -30,4 +52,29 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import "~@styles/_global.scss";
+
+.card-deck-container {
+  display: flex;
+  width: 100%;
+  justify-content: space-between;
+
+  overflow-x: auto;
+  overflow-y: hidden;
+  padding: 5px 0;
+  scroll-behavior: smooth;
+}
+
+@media (max-width: $SM) {
+  .card-deck-container {
+    flex-basis: auto;
+    flex-grow: 1;
+    flex-shrink: 1;
+    overflow-x: auto;
+  }
+}
+
+.card-deck-item {
+  flex-grow: 1;
+}
 </style>
