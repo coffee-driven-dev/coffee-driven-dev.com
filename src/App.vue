ffb23b520044847f6d4333855edd7f4535f8480c
dannyrb <danny.ri.brown@gmail.com>
2018-05-10 22:15:07 -0400

    Move all of blue container's handling up to app

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,24 @@
 <template>
   <div id="app">
 
-	<!-- NAV -->
-	<cdd-top-bar :collapsed="isNavCollapsed" />
+	<!-- BLUE SECTION -->
+	<div class="blue" :class="{ collapsed: isNavCollapsed }">
+		<!-- NAV -->
+		<cdd-top-bar />
+
+		<!-- HERO CTA -->
+		<transition name="fade">
+			<cdd-row v-if="!isNavCollapsed" style="justify-content: space-between; flex-wrap: wrap;">
+				<div class="hero-container">
+					<h1 style="margin: 10px 0; color: #FFF; font-size: 33px;">Community. Lorem Ipsum. Education. Fun!</h1>
+				</div>
+				<div class="graphic-container">
+					<cdd-twitch-player />
+
+				</div>
+			</cdd-row>
+		</transition>
+	</div>
 
 	<!-- PAGE -->
 	<!-- <cdd-app-navigation /> -->
@@ -20,6 +36,7 @@
 import { mapState } from "vuex";
 import CddAppNavigation from "./components/AppNavigation.vue";
 import CddTopBar from "./components/TopBar.vue";
+import CddTwitchPlayer from "@components/TwitchPlayer.vue";
 import CddFooter from "@components/Footer.vue";
 
 export default {
@@ -27,6 +44,7 @@ export default {
   components: {
     CddAppNavigation,
     CddTopBar,
+    CddTwitchPlayer,
     CddFooter
   },
   computed: {
@@ -49,6 +67,8 @@ export default {
 body {
   background: $background;
   color: #5c5c5c;
+  min-width: 350px;
+  overflow-x: hidden;
 }
 
 // App
@@ -58,6 +78,9 @@ body {
   -moz-osx-font-smoothing: grayscale;
 }
 
+/*
+ * PAGE Transition
+*/
 .page-enter-active {
   transition: opacity 0.25s ease-out;
 }
@@ -68,6 +91,50 @@ body {
 
 .page-enter,
 .page-leave-active {
+  opacity: 0;
+}
+
+// Blue container
+.blue {
+  background: $primary;
+  padding-bottom: 150px;
+  width: 100%;
+  overflow: hidden;
+  max-height: 600px;
+
+  transition-delay: 0.15s;
+  transition-duration: 0.3s;
+  transition-property: max-height, padding-bottom;
+  transition-timing-function: ease-in-out;
+
+  &.collapsed {
+    overflow: hidden;
+    max-height: 75px;
+    padding-bottom: 0;
+  }
+}
+
+.hero-container {
+  width: 50%;
+  justify-content: center;
+  display: flex;
+  flex-direction: column;
+}
+
+.graphic-container {
+  align-items: flex-end;
+  min-width: 400px; // Vid player min
+  width: 37.5%;
+  display: flex;
+  flex-direction: column;
+  margin: 25px 0 10px 0;
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s;
+}
+.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
   opacity: 0;
 }
 </style>
